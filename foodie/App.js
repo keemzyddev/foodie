beea3ceed48a3ec8e47b3b3782d1ac744f55fd8a
keemzyddev <hakeemdurogbola@gmail.com>
2023-11-22 12:05:17 +0100

    first commit

---
+++ foodie/App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from "./screens/HomeScreen";
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ foodie/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx}",
+    "./screens/**/*.{js,jsx,}",
+    "./components/**/*.{js,jsx,}",
+   "./src/**/*.{js,jsx,}"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
